4a2f5c88fb67e3d87e8d6cc7262daa26b7a1bd15
gnomezgrave <gnomez.grave@gmail.com>
2013-05-16 22:43:20 +0530

    Segmentation fault.

--- ipvcplayer.cpp
+++ ipvcplayer.cpp
@@ -27,16 +27,28 @@ void ipvcPlayer::on_browse_clicked()
 }
 
 void ipvcPlayer::readFile(QString file){
+
+    namedWindow("Output", CV_WINDOW_AUTOSIZE );
+
     FILE *ipvc_file = fopen(file.toStdString().c_str(), "r");
-
-
-
-
     ipvc_file_header_t *fh = (ipvc_file_header_t*) malloc(sizeof(ipvc_file_header_t));
     fread(fh,1,sizeof(ipvc_file_header_t),ipvc_file);
+
+    Mat output(fh->height,fh->width,CV_8UC3);
+
     char type;
     while(fread(&type,1,sizeof(char),ipvc_file)){
         if((char)type==126){
+            for(int i=0;i<fh->width;i++){
+                for(int j=0;j<fh->height;j++){
+                    char r,g,b;
+                    fread(&r,1,1,ipvc_file);
+                    fread(&g,1,1,ipvc_file);
+                    fread(&b,1,1,ipvc_file);
+                    output.at<Vec3b>(i,j)=Vec3b(r,g,b);
+                }
+            }
+            imshow("Output", output);
 
         }else if((char)type==122){
             printf("Move\n");
@@ -52,5 +64,3 @@ int main(int argc, char *argv[])
     w.show();
     return a.exec();
 }
-
-
